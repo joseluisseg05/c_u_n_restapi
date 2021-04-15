095abf00d9ece8120cc8b9441a204b7b1f1ce39d
joseluisseg05 <joseluistimy@gmail.com>
2021-04-14 21:22:58 -0500

    CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //conetar a mongo 
 mongoose.Promise = global.Promise;
@@ -15,6 +16,9 @@ const app = express();
 //bodyParser 
 app.use(express.urlencoded({extends: true})); //datos que vienen de un formulario html 
 app.use(express.json()); // datos de un json
+
+//cors
+app.use(cors())
 
 app.use('/', routes())
 
