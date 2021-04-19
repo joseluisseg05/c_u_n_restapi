c9e7aff98c8cbd88193757e673c643aa88bce86b
joseluisseg05 <joseluistimy@gmail.com>
2021-04-18 21:40:52 -0500

    Variables de entorno

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config({path: '.env'});
 
 //conetar a mongo 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/restapi', {
+mongoose.connect(process.env.BD_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -18,11 +19,28 @@ const app = express();
 app.use(express.urlencoded({extends: true})); //datos que vienen de un formulario html 
 app.use(express.json()); // datos de un json
 
+//dominio en lista blanca
+const whiteList = [process.env.FRONTEND_URL];
+const corsOptions = {
+    origin: (origin, callback) => {
+        //revisar si viene de una url de la lista blanca
+        const existe = whiteList.some(dominio => dominio === origin);
+        if(existe){
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
 //cors
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/', routes())
 
 app.use(static('uploads'));
 
-app.listen(8080)
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 8080
+app.listen(port, host, () => {
+    console.log('Servidor online')
+})
